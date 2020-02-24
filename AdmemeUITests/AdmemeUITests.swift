455343a0526ddc0c956f6ebfa36db3d91f273578
jhovan <jhovan.gallardo@gmail.com>
2020-02-24 02:12:31 -0600

    UITests added

--- AdmemeUITests/AdmemeUITests.swift
+++ AdmemeUITests/AdmemeUITests.swift
@@ -10,26 +10,45 @@ import XCTest
 
 class AdmemeUITests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var url: URL?
+    
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
+    func testVerTodo() {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(app.navigationBars["Admeme"].buttons["Add"].exists)
+        XCTAssert(app.navigationBars["Admeme"].buttons["Seleccionar"].exists)
+    }
+    
+    func testFavoritos() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        app.tabBars.buttons["Favoritos"].tap()
+        XCTAssert(app.navigationBars["Tus Favoritos"].buttons["Seleccionar"].exists)
+        
+    }
+    
+    func testCategorias() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        XCTAssert(app.tabBars.buttons["Agrupar"].exists)
+    }
+    
+    func testSeleccionar() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.navigationBars["Admeme"].buttons["Seleccionar"].tap()
+        
+        let element = app.toolbars["Toolbar"].children(matching: .other).element.children(matching: .other).element
+        
+        XCTAssert(element.children(matching: .button).matching(identifier: "Item").element(boundBy: 1).exists)
+        XCTAssert(element.children(matching: .button).matching(identifier: "Item").element(boundBy: 0).exists)
+        
     }
 
     func testLaunchPerformance() {
